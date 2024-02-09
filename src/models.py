1681f93844a86ce8648b261c1d8b249e716f52ca
Michael Lee <michaellee1019@gmail.com>
2024-02-09 16:24:38 -0500

    E->e

--- src/models.py
+++ src/models.py
@@ -56,10 +56,10 @@ class RoborioNetworkTableSensorServer(Generic):
             for (sensor_name, sensor_resource) in self.sensors:
                 #try:
                     readings = await sensor_resource.get_readings()
-                    LOGGER.Error("readings: ", readings)
+                    LOGGER.error("readings: ", readings)
                     for (reading_name, reading) in readings.items():
-                        LOGGER.Error("reading_name: ", reading_name)
-                        LOGGER.Error("reading: ", reading)
+                        LOGGER.error("reading_name: ", reading_name)
+                        LOGGER.error("reading: ", reading)
                         self.nt.putValue("{}-{}".format(sensor_name, reading_name), reading.value_to_primitive())
                 #except Exception as e:
                 #    LOGGER.error("failed to update network table: {}".format(e))
